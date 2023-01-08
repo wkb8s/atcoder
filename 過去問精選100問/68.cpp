03655d6989c1ebc0050abf8e2c38c2bdea1c58da
waka <apollo.daiki@gmail.com>
2023-01-08 12:48:03 +0900

    solved

---
+++ 過去問精選100問/68.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << n << ":";
+    int N = n;
+    for(int i=2; i<sqrt(n); i++){
+        while(N%i == 0){
+            cout << " " << i;
+            N /= i;
+        }
+    }
+    if(N != 1){
+        cout << " " << N;
+    }
+    cout << endl;
+}
