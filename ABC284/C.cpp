42f8089fe40dda87bf557ea03d03001c9f987633
waka <apollo.daiki@gmail.com>
2023-01-10 12:43:37 +0900

    resolved

--- ABC284/C.cpp
+++ ABC284/C.cpp
@@ -1,41 +1,46 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+#define MAX 100
+#define UNVISITED -1
+#define VISITED 1
 using namespace std;
-using Graph = vector<vector<int>>;
 
-// 深さ優先探索
-vector<bool> seen;
-void dfs(const Graph &G, int v) {
-    seen[v] = true; // v を訪問済にする
+int cnt = 0;
+int N, M;
+int color[MAX];
+vector<int> G[MAX];
 
-    // v から行ける各頂点 next_v について
-    for (auto next_v : G[v]) { 
-        if (seen[next_v]) continue; // next_v が探索済だったらスルー
-        dfs(G, next_v); // 再帰的に探索
+void dfs_visit(int u){
+    color[u] = VISITED;
+    for(int i=0; i<G[u].size(); i++){
+        int v = G[u][i];
+        if(color[v] == UNVISITED){
+            dfs_visit(v);
+        }
+    }
+}
+
+void dfs(){
+    for(int i=0; i<N; i++){
+        if(color[i] == UNVISITED){
+            dfs_visit(i);
+            cnt++;
+        }
     }
 }
 
 int main() {
-    // 頂点数と辺数
-    int N, M; cin >> N >> M;
-
-    // グラフ入力受取 (ここでは無向グラフを想定)
-    Graph G(N);
-    for (int i = 0; i < M; ++i) {
-        int a, b;
-        cin >> a >> b;
-        G[a].push_back(b);
-        G[b].push_back(a);
+    cin >> N >> M;
+    for(int i=0; i<M; i++){
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+        G[u].push_back(v);
+        G[v].push_back(u);
     }
-
-    // 頂点 0 をスタートとした探索
-    int cnt=0;
-    seen.assign(N, false); // 全頂点を「未訪問」に初期化
-    for (int i = 0; i < N; i++) {
-        if (!seen[i]) {
-            dfs(G, i);
-            cnt++; // dfsした回数をカウント
-        }
+    for(int i=0; i<N; i++){
+        color[i] = UNVISITED;
     }
+    dfs();
     cout << cnt << endl;
 }
